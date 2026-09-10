4d7a7bf40f282392b19898ada07c8a1e039409c8
ForceNext <hjslyx00@163.com>
2026-09-10 19:39:58 +0800

    测试了yaml

--- delta_bottom/user/main.cpp
+++ delta_bottom/user/main.cpp
@@ -1,9 +1,18 @@
 #include "iostream"
 #include "Utilities/PeriodicTask.h"
 #include "yaml-cpp/yaml.h"
+#include <vector>
+#include <string>
+#include <cstdint>
 
-int main()
+int main(int argc, char** argv)
 {
+    std::string path = (argc > 1) ? argv[1] : "config/Drive_Param.yaml";
+    YAML::Node root = YAML::LoadFile(path);
+    auto serial = root["serial"];
+    std::string port = serial["port"].as<std::string>();
+    int baudrate     = serial["baudrate"].as<int>();
 
+    std::cout << "===== serial =====\n"<< "  port     = " << port << "\n"<< "  baudrate = " << baudrate << "\n";
     return 0;
 }
